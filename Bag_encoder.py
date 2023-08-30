d8cc91bb98d3fc8cbec5b44f2117f21f35c06bd9
Samira Korani <S.Korani1@nuigalway.ie>
2023-08-30 11:32:40 +0100

    Update Bag_encoder.py

--- Bag_encoder.py
+++ Bag_encoder.py
@@ -14,6 +14,13 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # batch_size = 32
 
 
+from collections import defaultdict
+from transformers import BertTokenizer, BertModel
+import torch.nn as nn
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class BagEncoder:
     def __init__(self, pretrained_model_name, batch_size):
         self.tokenizer = BertTokenizer.from_pretrained(pretrained_model_name)
@@ -27,22 +34,40 @@ class BagEncoder:
         ).to(device)
 
     def encode_sentences(self, sentences, labels, entity_pairs):
-        inputs = self.tokenizer(sentences, return_tensors="pt", padding=True, truncation=True, max_length=32).to(
-            device)
-        outputs = self.bert_model(**inputs)
-        sentence_embedding = torch.mean(outputs.last_hidden_state, dim=1).to(device)
-        data = self.create_bags(sentence_embedding, labels, entity_pairs)
+        entity_pair_keys = [(min(ep0, ep1), max(ep0, ep1)) for ep0, ep1 in zip(entity_pairs[0], entity_pairs[1])]
+        grouped_data = defaultdict(lambda: {"sentences": [], "labels": [], "entity_pairs": []})
+
+        # Group sentences, labels, and entity pairs by their respective entity pair keys
+        for sentence, label, key in zip(sentences, labels, entity_pair_keys):
+            grouped_data[key]['sentences'].append(sentence)
+            grouped_data[key]['labels'].append(label)
+            grouped_data[key]['entity_pairs'].append(key)
+
+        data = {}
+        for key, group in grouped_data.items():
+            data[key] = self.process_group(group['sentences'], group['labels'], group['entity_pairs'])
+        
         return data
 
-    def create_bags(self, sentence_embeddings, labels, entity_pairs):
+    def process_group(self, group_sentences, group_labels, group_entity_pairs):
+        num_samples = len(group_sentences)
         data = defaultdict(lambda: defaultdict(list))
-        for sentence_embedding, label, entity_pair_0, entity_pair_1, in zip(sentence_embeddings,
-                                                                            labels,
-                                                                            entity_pairs[0],
-                                                                            entity_pairs[1]):
 
-            if entity_pair_0 >= entity_pair_1:
-                entity_pair_1, entity_pair_0 = entity_pair_0, entity_pair_1
+        for i in range(0, num_samples, self.batch_size):
+            batch_sentences = group_sentences[i:i+self.batch_size]
+            batch_labels = group_labels[i:i+self.batch_size]
+            batch_entity_pairs = group_entity_pairs[i:i+self.batch_size]
+
+            inputs = self.tokenizer(batch_sentences, return_tensors="pt", padding=True, truncation=True, max_length=32).to(device)
+            outputs = self.bert_model(**inputs)
+            sentence_embedding = torch.mean(outputs.last_hidden_state, dim=1).to(device)
+            self.create_bags(data, sentence_embedding, batch_labels, batch_entity_pairs)
+            
+        return data
+
+    def create_bags(self, data, sentence_embeddings, labels, entity_pairs):
+        for sentence_embedding, label, (entity_pair_0, entity_pair_1) in zip(sentence_embeddings, labels, entity_pairs):
+
             data[(entity_pair_0, entity_pair_1)]['embeddings'].append(sentence_embedding)
             data[(entity_pair_0, entity_pair_1)]['labels'].append(label)
             data[(entity_pair_0, entity_pair_1)]['entity_pairs'].append((entity_pair_0, entity_pair_1))
@@ -52,14 +77,10 @@ class BagEncoder:
             attention_weights = self.attention(bag)  # shape: (seq_len, 1)
             attention_weights = torch.softmax(attention_weights, dim=0)  # take softmax over seq_len dimension
             attention_applied = (bag * attention_weights)  # shape: (hidden_dim,)
-            # attention_weights_1D = attention_weights.view(-1)  # This reshapes it into a 1D tensor.
-            # corr = torch.outer(attention_weights_1D, attention_weights_1D)
-
-            # corr = torch.outer(attention_weights, attention_weights)  # shape: (seq_len, seq_len)
+            
             data[pair_key]['bag'] = bag
             data[pair_key]['relation representation'] = attention_applied
 
-            # data[corr]['Instant Corelation'] = corr
         return data
 
 # bag_encoder = BagEncoder(pretrained_model_name="bert-base-uncased", batch_size=batch_size)
